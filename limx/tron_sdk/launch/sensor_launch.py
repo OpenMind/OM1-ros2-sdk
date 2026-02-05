7ea73ebb4d2e095094786e075b21097a729eade2
jerinpeter <jerinpeter@gmail.com>
2026-02-04 18:26:07 -0800

    update-static-transform-for-limx

--- limx/tron_sdk/launch/sensor_launch.py
+++ limx/tron_sdk/launch/sensor_launch.py
@@ -38,9 +38,40 @@ def generate_launch_description():
         if not (
             isinstance(e, Node)
             and hasattr(e, "_Node__node_name")
-            and e._Node__node_name == "d435_obstacle_dector"
+            and e._Node__node_name
+            in ["d435_obstacle_dector", "static_transform_publisher_camera"]
         )
     ]
+
+    entities.append(
+        Node(
+            package="tf2_ros",
+            executable="static_transform_publisher",
+            name="static_transform_publisher_camera",
+            arguments=[
+                "--x",
+                "0.2",
+                "--y",
+                "0",
+                "--z",
+                "0.10",
+                "--roll",
+                "0",
+                "--pitch",
+                "0.610865",
+                "--yaw",
+                "0",
+                "--frame-id",
+                "base_link",
+                "--child-frame-id",
+                "camera_link",
+            ],
+            output="screen",
+            respawn=True,
+            respawn_delay=2.0,
+            condition=UnlessCondition(use_sim),
+        ),
+    )
 
     entities.extend(
         [
